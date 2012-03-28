aca9fd1734182d0a3e45521a9f9d0eb72537cc17
Anand <anand@heroku.com>
2012-03-27 18:36:11 -0700

    Added Test Page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,13 +12,17 @@ import play.mvc.Controller;
 
 public class Application extends Controller {
 
-	public static void index(String zipCode,String jsonCallBack) throws SAXException, IOException, ParserConfigurationException {
+	public static void index(String zipCode,String jsoncallBack) throws SAXException, IOException, ParserConfigurationException {
     	String rate = FedexServicesHelper.getShippingRateFor(zipCode);
-        if(jsonCallBack!=null && jsonCallBack.trim().length()>0){
+        if(jsoncallBack!=null && jsoncallBack.trim().length()>0){
         	response.contentType="application/x-javascript";
-        	renderTemplate("Application/index.jsonp",jsonCallBack,rate);
+        	renderTemplate("Application/index.jsonp",jsoncallBack,rate);
         }else{
         	render(rate);
         }
     }
+	
+	public static void testJsonP(){
+		render();
+	}
 }
